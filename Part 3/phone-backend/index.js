5ac9c82e661dc4cf7abdd91b48864a705f2bf355
alejandrosanesteban <alejandrosanesteban@gmail.com>
2024-07-02 06:08:12 +0200

    added cors

--- Part 3/phone-backend/index.js
+++ Part 3/phone-backend/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 const app = express();
+const cors = require('cors')
 
 app.use(express.json());
 // app.use(morgan('tiny'));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+app.use(cors())
 
 morgan.token('body', (request) => {
     return JSON.stringify(request.body);
@@ -92,7 +94,7 @@ app.post('/api/persons', (request,response) => {
 })
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
